8b946d65baba2e0856b92ec82e8349a5a9a5d2e5
AnaFainberg <138641804+AnaFainberg@users.noreply.github.com>
2024-05-08 08:52:43 -0300

    corrección en función de intro

--- app.js
+++ app.js
@@ -5,15 +5,3 @@ document.addEventListener('DOMContentLoaded', (e) => {
         intro.classList.add('display-none');
     }, 5000);
 })
-
-const header = document.querySelector('.header');
-
-window.onscroll = function(){
-    var top = window.scrollY;
-    console.log(top);
-    if (top >=50){
-        header.classList.add('active')
-    }else{
-        header.classList.remove('active');
-    }
-}
