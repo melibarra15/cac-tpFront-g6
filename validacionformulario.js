92b8ca160eb710ec9d9b4e198413bfa21a0fdf21
AnaFainberg <138641804+AnaFainberg@users.noreply.github.com>
2024-05-15 10:30:41 -0300

    intentando agregar js a form (voy a consultar)

---
+++ validacionformulario.js
@@ -0,0 +1,10 @@
+//validar formulario//
+
+
+
+
+//pantalla de agradecimiento//
+function agradecer() {
+    var gracias = document.getElementsByClassName(".gracias");
+    gracias.styleList.add(".agradecidas");
+}
