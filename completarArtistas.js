fe53165502b5bb34bff1a077f9396e970558bfd9
Melanie Ibarra <meluibarra15@gmail.com>
2024-05-28 18:32:52 -0300

    script para llenar la pagina de artistas

--- completarArtistas.js
+++ completarArtistas.js
@@ -1,11 +1,42 @@
-let perfilArtista = document.querySelector('.perfil-de-artista');
-let modeloPerfilArtista = perfilArtista.cloneNode(true);
+let perfilDeArtista = document.querySelector('.perfil-de-artista');
+let modeloPerfilArtista = perfilDeArtista.cloneNode(true);
+perfilDeArtista.remove();
+
+agregarPerfil();
 
 function agregarPerfil(){
-    fetch("https://melibarra15.github.io/cac-tpFront-g6/datos.json")
+    fetch("https://melibarra15.github.io/cac-tpFront-g6/artistas.json")
     .then(response => response.json())
     .then(data => {
-        
+        for (i=0; i<data.length; i++){
+            perfilArtista = modeloPerfilArtista.cloneNode(true);
+
+            perfilArtista.querySelector("#foto-artista").src = "#";
+            perfilArtista.querySelector("#nombre-artista").innerHTML = data[i].nombre
+            
+            let contenedor = buscadorDeSeccion(data[i].profesion);
+            contenedor.appendChild(perfilArtista);
+        }
         })
     .catch(error => console.log("Ocurrió un error! " + error));
+}
+
+function buscadorDeSeccion(seccion){
+    switch (seccion){
+        case 'escultura':
+            contenedor = document.querySelector("#escultura");
+            break;
+        case 'fotografia':
+            contenedor = document.querySelector("#fotografia");
+            break;
+        case 'ceramista':
+            contenedor = document.querySelector("#ceramica");
+            break;
+        case 'ilustrador':
+            contenedor = document.querySelector("#ilustracion");
+            break;
+        default:
+            console.error("la profesion del artista no tiene una categoria correspondiente");
+    }
+    return contenedor;
 }
