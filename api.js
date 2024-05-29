10419fff514dd14d99545f60c9c0a43d6a657b13
AnaFainberg <138641804+AnaFainberg@users.noreply.github.com>
2024-05-29 03:47:05 -0300

    chala-head-chala 2

--- api.js
+++ api.js
@@ -1,22 +1,32 @@
 let sticker = document.querySelector("#sticker");
 let stickerCopia = sticker.cloneNode(true);
-
+let main = document.querySelector("main");
 let contenedor = document.querySelector("#container-stickers");
 
 sticker.remove()
 
-fetch("https://dragonball-api.com/api/characters")
-.then(response => response.json())
-.then(data => {
+function AgregarStickers(){
 
-    //buscando contenedor y creando child figure
-    let nuevoSticker = stickerCopia.cloneNode(true);
+    fetch("https://dragonball-api.com/api/characters")
+    .then(response => response.json())
+    .then(data => {
+               //iterando por cada elemento del array del json
+           for(let i = 0; i<= data.items.length; i++){
+            //buscando contenedor y creando child figure
+            let nuevoSticker = stickerCopia.cloneNode(true);
 
-    nuevoSticker.querySelector("#imagensticker").src = data.items[0].image;
-    nuevoSticker.querySelector("#imagensticker").alt = "Sticker de"+" "+data.items[0].name;
-    nuevoSticker.querySelector("#sticker #capa #nombresticker").textContent = data.items[0].name;
-    
-    contenedor.appendChild(nuevoSticker);
-    
+            nuevoSticker.querySelector("#imagensticker").src = data.items[i].image;
+            nuevoSticker.querySelector("#imagensticker").alt = "Sticker de"+" "+data.items[i].name;
+            nuevoSticker.querySelector("#sticker #capa #nombresticker").textContent = data.items[i].name;
+            //creando nuevo sticker con la nueva imagen y nombre
+            contenedor.appendChild(nuevoSticker);
+
+        }
     })
-.catch(error => console.log("Ocurrió un error!"));
+    .catch(error => console.log("Ocurrió un error!")); 
+}
+//que esto se de al abrir la página
+window.addEventListener("load", AgregarStickers);
+
+
+
