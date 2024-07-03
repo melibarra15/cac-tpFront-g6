941800d93806d9fcb903de8672495404c6dffd09
AnaFainberg <138641804+AnaFainberg@users.noreply.github.com>
2024-07-03 09:24:01 -0300

    boton login en otras páginas

---
+++ fetchData.js
@@ -0,0 +1,17 @@
+function fetchData(url, method, callback, data_request = null){
+    const opctions = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: data_request ? JSON.stringify(data_request) : null, //Si hay datos, los convierte a JSON y los incluye en el cuerpo
+    };
+
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        callback(data);
+    })
+    .catch(error => console.log("Ocurrió un error!" + error));
+}
+
